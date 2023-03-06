53deb950ee596bfdbfb76f1e36ab37f3345fe6f8
lzhao <lzhao@pcienergysolutions.com>
2023-03-05 22:16:14 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-// ! 来个列表, 通过map函数来渲染列表
-// * sdw 
-// todo 
-const songs = [
-  { id: 1, name: "前端性能优化设计技巧" },
-  { id: 2, name: "自顶向下学React源码" },
-  { id: 3, name: "React的社区的组成" },
-];
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -26,12 +17,6 @@ function App() {
         >
           Learn React
         </a>
-        <p> 目录 !!!!</p>
-        <ul>
-          {songs.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
-// React 框架的核心包
 import React from 'react';
-// ReactDOM 渲染相关的包
 import ReactDOM from 'react-dom/client';
-// 应用的全局样式文件
 import './index.css';
-// 根组件
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// 渲染根组件App到id为root的dom节点上
-// public目录下有个index.html文件 上面能找到root节点
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // 严格模式节点需要去掉
-  // useEffect的执行时机
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-//index.js是链接react与html模板的桥梁，所有的react组件最终都是由他进行渲染到html模板中。
-//然后整个文件中引入了一个系统默认生成的组件App，由index.js进行渲染。
 
 reportWebVitals();
