acb61596d3e7059a6f86065f468557b2e1dfac17
lzhao <lzhao@pcienergysolutions.com>
2023-03-05 22:11:59 -0500

    new commit (need to revert)

---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    state = {  } 
+    render() { 
+        return (<p> 目录 !!!!</p>);
+    }
+}
+ 
+export default Test;
